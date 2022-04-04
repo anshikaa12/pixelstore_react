732fe7146008a3f625c6bab8cf03a1ad1e28246c
Anshika Agarwal <agarwal.anshika100@gmail.com>
2022-04-04 18:24:10 +0530

    used prettier

--- src/reducer/filterreducer.js
+++ src/reducer/filterreducer.js
@@ -1,30 +1,40 @@
-import { useProduct } from "../services/product_api"
+import {
+    useProduct
+} from "../services/product_api"
 
 
-function filterReducer(state,action){
-switch(action.type){
-    case "SORT_PRICE":
-        return {...state,sortBy:action.payload}
-    case "SORT_CATEGORY":
-        return {...state,categories:state.categories.includes(action.payload)?state.categories.filter(item=>item!==action.payload):[...state.categories,action.payload]}
-}
-}
-
-function sorted_price_list(sortBy,cardData){
-
-if(sortBy == "HIGH_TO_LOW"){
-    return [...cardData].sort((a, b) => b["price"] - a["price"]);
-}
-if(sortBy == "LOW_TO_HIGH"){
-    return [...cardData].sort((a, b) => a["price"] - b["price"]);
-}
-return cardData;
-
+function filterReducer(state, action) {
+    switch (action.type) {
+        case "SORT_PRICE":
+            return {
+                ...state, sortBy: action.payload
+            }
+            case "SORT_CATEGORY":
+                return {
+                    ...state, categories: state.categories.includes(action.payload) ? state.categories.filter(item => item !== action.payload) : [...state.categories, action.payload]
+                }
+    }
 }
 
-function sorted_category_list(categories,cardData){
-   let updatedList= [...cardData].filter(item=>categories.includes(item.category));
-   return updatedList;
+function sorted_price_list(sortBy, cardData) {
+
+    if (sortBy == "HIGH_TO_LOW") {
+        return [...cardData].sort((a, b) => b["price"] - a["price"]);
+    }
+    if (sortBy == "LOW_TO_HIGH") {
+        return [...cardData].sort((a, b) => a["price"] - b["price"]);
+    }
+    return cardData;
+
 }
 
-export {filterReducer,sorted_price_list, sorted_category_list}
+function sorted_category_list(categories, cardData) {
+    let updatedList = [...cardData].filter(item => categories.includes(item.category));
+    return updatedList;
+}
+
+export {
+    filterReducer,
+    sorted_price_list,
+    sorted_category_list
+}
--- src/services/category_api.js
+++ src/services/category_api.js
@@ -7,6 +7,10 @@ import axios from "axios";
 function useCategory() {
     const [category, setCategory] = useState([]);
     useEffect(() => axios.get('/api/categories').then(resp => setCategory(resp.data.categories).catch(err => console.error(err))), []);
-    return {category}
+    return {
+        category
+    }
 }
-export {useCategory};
+export {
+    useCategory
+};
